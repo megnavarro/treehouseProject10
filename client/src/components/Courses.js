2617ac54c2deca3930bace983aa53fd72893c8fe
megnavarro <65045355+megnavarro@users.noreply.github.com>
2020-10-27 14:16:37 -0400

    Initial attempt

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-const Courses = props => { 
+const Courses = (props) => { 
+    const [courses, setCourses] = useState([]);
+    const { context } = this.props;
 
+    useEffect(() => {
+        
+        context.data.api('/courses')
+            .then(data => setCourses(data))
+            .catch(error => console.log('Error fetching and parsing data', error))
+    }, []);
+
+    return (
+        <div className="bounds">
+        {courses}
+        <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
+            <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 13 13" className="add">
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+              </svg>New Course</h3>
+          </a></div>
+      </div>
+    )
 }
 
 export default Courses;
